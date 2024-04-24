c4b9ac3e0ee5881996eedd454915e8fc13ab36ed
Keidson Roby <keidsondesigner@gmail.com>
2024-04-24 19:30:57 -0400

    feat: :sparkles: Criado TicketService com método buscaPorId()

--- src/main/java/br/com/keidsonroby/helpdesk/modules/tickets/service/TicketService.java
+++ src/main/java/br/com/keidsonroby/helpdesk/modules/tickets/service/TicketService.java
@@ -22,4 +22,8 @@ public class TicketService {
     var ticket = this.ticketRepository.findAll();
     return ticket;
   }
+
+  public TicketEntity buscaPorId(Integer id) {
+    return this.ticketRepository.findById(id).get();
+  }
 }
